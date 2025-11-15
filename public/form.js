11823cf5d237e85bc6284ea65e0d2331e404b8f7
Joona Niemenmaa <joonaoskari@hotmail.com>
2025-11-15 15:55:21 +0200

    tehtävä 2

--- public/form.js
+++ public/form.js
@@ -1,7 +1,12 @@
 const todo_form = document.getElementById("todoForm");
+const search_form = document.getElementById("searchForm");
+
 const name_input = document.getElementById("userInput");
 const todo_input = document.getElementById("todoInput");
-const response_paragraph = document.getElementById("response_paragraph");
+const search_input = document.getElementById("searchInput");
+
+const todo_paragraph = document.getElementById("todo_paragraph");
+const search_paragraph = document.getElementById("search_paragraph");
 
 todo_form.addEventListener("submit", async (event) => {
 	event.preventDefault();
@@ -30,5 +35,37 @@ todo_form.addEventListener("submit", async (event) => {
 
 	const response = await (await fetch(url, options)).text();
 
-	response_paragraph.innerText = response;
+	todo_paragraph.innerText = response;
+});
+
+function display_todos(todos) {
+	const todo_list = document.getElementById("todo_list");
+	todo_list.innerHTML = "";
+	for (const todo of todos) {
+		const li = document.createElement("li");
+		li.innerText = todo;
+		todo_list.appendChild(li);
+	}
+}
+
+search_form.addEventListener("submit", async (event) => {
+	event.preventDefault();
+
+	const query = search_input.value;
+	if (query) {
+		const url = `http://localhost:3000/todos/${query}`;
+
+		const response = await fetch(url);
+		const content_type = response.headers.get("Content-Type");
+		const body = await response.text();
+
+		if (content_type.startsWith("application/json")) {
+			const todos = JSON.parse(body);
+			console.log(todos);
+			search_paragraph.innerText = "";
+			display_todos(todos);
+		} else if (content_type.startsWith("text/html")) {
+			search_paragraph.innerText = body;
+		}
+	}
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, response } from "express";
 
 const router = Router();
 
@@ -37,6 +37,22 @@ router.post("/add", (request: Request, response: Response) => {
 		response.send(`Todo added successfully for user ${request_body.name}.`);
 	} else {
 		response.send(`Todo or user name cannot be empty!`);
+	}
+});
+
+router.get("/todos/:id", (request: Request, response: Response) => {
+	const name = request.params.id;
+	let todos: string[] | undefined = undefined;
+	for (const user of users) {
+		if (name === user.name) {
+			todos = user.todos;
+		}
+	}
+
+	if (todos !== undefined) {
+		response.send(todos);
+	} else {
+		response.send("User not found");
 	}
 });
 
